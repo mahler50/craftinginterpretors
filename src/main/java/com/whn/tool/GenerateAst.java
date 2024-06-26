3d3789534226dc21cfd143b10881a906f957126b
whn <1874695345@qq.com>
2024-06-26 17:34:41 +0800

    初步抽象表达式以及生成抽象语法树

---
+++ src/main/java/com/whn/tool/GenerateAst.java
@@ -0,0 +1,112 @@
+package com.whn.tool;
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.List;
+
+public class GenerateAst {
+
+    public static void main(String[] args) throws IOException {
+        if (args.length != 1) {
+            System.err.println("Usage: generate_ast <output directory>");
+            System.exit(64);
+        }
+
+        String outputDir = args[0];
+        defineAst(outputDir, "Expr", Arrays.asList(
+                "Binary   : Expr left, Token operator, Expr right",
+                "Grouping : Expr expression",
+                "Literal  : Object value",
+                "Unary    : Token operator, Expr right"
+        ));
+    }
+
+    /**
+     * 定义抽象语法树
+     * @param outputDir 输出文件夹
+     * @param baseName 文件名
+     * @param types 子类类型
+     * @throws FileNotFoundException
+     * @throws UnsupportedEncodingException
+     */
+    private static void defineAst(String outputDir, String baseName, List<String> types) throws FileNotFoundException, UnsupportedEncodingException {
+        String path = outputDir + File.separator + baseName + ".java";
+        PrintWriter writer = new PrintWriter(path, "UTF-8");
+
+        writer.println("package com.whn.lox;");
+        writer.println();
+        writer.println("import java.util.List;");
+        writer.println();
+        writer.println("abstract class " + baseName + " {");
+
+        defineVisitor(writer, baseName, types);
+
+        for (String type : types) {
+            String className = type.split(":")[0].trim();
+            String fields = type.split(":")[1];
+            defineType(writer, baseName, className, fields);
+        }
+
+        // The base accept() method.
+        writer.println();
+        writer.println("  abstract <R> R accept(Visitor<R> visitor);");
+        writer.println("}");
+        writer.close();
+    }
+
+    /**
+     * 定义抽象语法树类
+     * @param writer
+     * @param baseName
+     * @param className
+     * @param fieldList
+     */
+    private static void defineType(PrintWriter writer, String baseName, String className, String fieldList) {
+        writer.println("  static class " + className + " extends " +
+                baseName + " {");
+
+        // Constructor.
+        writer.println("    " + className + "(" + fieldList + ") {");
+        String[] fields = fieldList.split(", ");
+        for (String field : fields) {
+            String name = field.trim().split(" ")[1];
+            writer.println("      this." + name + " = " + name + ";");
+        }
+
+        writer.println("    }");
+
+        // Visitor pattern.
+        writer.println();
+        writer.println("    @Override");
+        writer.println("    <R> R accept(Visitor<R> visitor) {");
+        writer.println("      return visitor.visit" +
+                className + baseName + "(this);");
+        writer.println("    }");
+
+        // Fields.
+        writer.println();
+        for (String field : fields) {
+            writer.println("    final " + field + ";");
+        }
+
+        writer.println("  }");
+    }
+
+    /**
+     * 定义访问器
+     * @param writer
+     * @param baseName
+     * @param types
+     */
+    private static void defineVisitor(PrintWriter writer, String baseName, List<String> types) {
+        writer.println("  interface Visitor<R> {");
+
+        for (String type : types) {
+            String typeName = type.split(":")[0].trim();
+            writer.println("    R visit" + typeName + baseName + "(" +
+                    typeName + " " + baseName.toLowerCase() + ");");
+        }
+
+        writer.println("  }");
+    }
+}
